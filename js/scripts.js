e00406114dac16d43bb900ed2b1bc167210d7c4d
Jonathan Carlos and Matt Taylor <jonathan@>
2020-01-16 16:35:12 -0800

    add second radio button group

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,28 @@
       if (age === "child") {
         alert("Child");
       } else if (age === "youth") {
-        alert("youth");
+        alert("Youth");
       } else {
-        alert("old");
+        alert("Adult");
       }
-      
+    
+      $(".question").toggle();
+      $(".age").toggle();
 
+      event.preventDefault();
+    }));
+
+    $('input[name="question"]').on('change', (function(e){
+      var question = $("input:radio[name=question]:checked").val();
       
+      if (question === "hat-yes") {
+        alert("Then take it off");
+      } else if (question === "hat-no") {
+        alert("Great, leave it on. We sell lost hats in the back park.");
+      } else {
+        alert("Then we'll sell it");
+      }
+
       event.preventDefault();
     }));
 });
